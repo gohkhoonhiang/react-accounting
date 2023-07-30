1f31769e08ffdfffbc947121049acb3b3c19768a
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2023-07-30 13:15:59 +0800

    Setup transfer form

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import { AuthProvider } from './AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
     <BrowserRouter basename={'/'}>
       <Routes>
         <Route
@@ -54,7 +54,7 @@ root.render(
         </Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.Fragment>
 );
 
 // If you want to start measuring performance in your app, pass a function
