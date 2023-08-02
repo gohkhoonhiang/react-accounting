2dfcd8aed38ad8579d5c73860223538e0cba34ae
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2023-08-02 17:02:38 +0800

    Update budget sheet to use InfoCard component

--- src/pages/budget/BudgetSheet.js
+++ src/pages/budget/BudgetSheet.js
@@ -1,35 +1,36 @@
 import React from 'react';
-import { Card, Elevation } from '@blueprintjs/core';
 import { useBudgetSheet } from './Context';
+import InfoCard from '../../components/InfoCard';
 
 function BudgetSheet() {
   const budgetSheet = useBudgetSheet();
 
-  return (
-    <Card className="Page-form Budget-sheet" elevation={Elevation.TWO}>
-      <h3>Budget Sheet</h3>
-      <div className="Field-set">
-        <div className="title">Start Date</div>
-        <div className="value">{budgetSheet?.startDate || 'NA'}</div>
-      </div>
-      <div className="Field-set">
-        <div className="title">End Date</div>
-        <div className="value">{budgetSheet?.endDate || 'NA'}</div>
-      </div>
-      <div className="Field-set">
-        <div className="title">Total Budgeted</div>
-        <div className="value">{budgetSheet?.totalBudgeted?.toFixed(2) || '0.00'}</div>
-      </div>
-      <div className="Field-set">
-        <div className="title">Actual Allocated</div>
-        <div className="value">{budgetSheet?.actualAllocated?.toFixed(2) || '0.00'}</div>
-      </div>
-      <div className="Field-set">
-        <div className="title">Balance To Allocate</div>
-        <div className="value">{budgetSheet?.balancetoAllocate?.toFixed(2) || '0.00'}</div>
-      </div>
-    </Card>
-  );
+  const heading = 'Budget Sheet';
+
+  const fields = [
+    {
+      title: 'Start Date',
+      value: budgetSheet?.startDate || 'NA'
+    },
+    {
+      title: 'End Date',
+      value: budgetSheet?.endDate || 'NA'
+    },
+    {
+      title: 'Total Budgeted',
+      value: budgetSheet?.totalBudgeted?.toFixed(2) || '0.00'
+    },
+    {
+      title: 'Actual Allocated',
+      value: budgetSheet?.actualAllocated?.toFixed(2) || '0.00'
+    },
+    {
+      title: 'Balance to Allocate',
+      value: budgetSheet?.balancetoAllocate?.toFixed(2) || '0.00'
+    }
+  ];
+
+  return <InfoCard heading={heading} fields={fields}></InfoCard>;
 }
 
 export default BudgetSheet;
