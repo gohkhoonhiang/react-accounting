4c22ab6713f938e9d3521da969cf9152b4480227
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2023-08-02 20:48:19 +0800

    Transactions dialog

--- src/pages/account/Table.js
+++ src/pages/account/Table.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, Elevation } from '@blueprintjs/core';
+import React, { useState } from 'react';
+import { Card, Elevation, Button, Dialog, DialogBody, DialogFooter } from '@blueprintjs/core';
 import SimpleTable from '../../components/SimpleTable';
 import CollapseTable from '../../components/CollapseTable';
 import './Account.css';
@@ -12,19 +12,41 @@ function AccountsTable() {
       buckets: [
         {
           name: 'Daily expenses',
-          amount: 829.29
+          amount: 829.29,
+          transactions: [
+            {
+              description: 'Kopitiam',
+              transactionDate: '2023-01-12',
+              amount: 7.23
+            }
+          ]
         },
         {
           name: 'Vacation fund',
-          amount: 1725.41
+          amount: 1725.41,
+          transactions: []
         },
         {
           name: 'Health fund',
-          amount: 1934.72
+          amount: 1934.72,
+          transactions: []
         },
         {
           name: 'Savings',
-          amount: 15912.92
+          amount: 15912.92,
+          transactions: []
+        }
+      ],
+      transactions: [
+        {
+          description: 'Payment to google.com',
+          transactionDate: '2023-01-23',
+          amount: -21.42
+        },
+        {
+          description: 'Interest credited',
+          transactionDate: '2023-01-31',
+          amount: 112.73
         }
       ]
     },
@@ -34,15 +56,25 @@ function AccountsTable() {
       buckets: [
         {
           name: 'PC fund',
-          amount: 923.62
+          amount: 923.62,
+          transactions: []
         },
         {
           name: 'Games fund',
-          amount: 236.23
+          amount: 236.23,
+          transactions: []
         },
         {
           name: 'Savings',
-          amount: 23777.86
+          amount: 23777.86,
+          transactions: []
+        }
+      ],
+      transactions: [
+        {
+          description: 'Payment to grab.com',
+          transactionDate: '2023-01-16',
+          amount: 27.24
         }
       ]
     }
@@ -64,9 +96,11 @@ function AccountsTable() {
   const bucketActions = [
     {
       icon: 'eye-open',
-      click: (e, row, index) => {
+      click: (e, row) => {
         e.preventDefault();
-        alert(`Opened bucket row ${index}: ${row.name}`);
+        setBucketTransactions(row.transactions);
+        setBucketTransactionsDialogTitle(`${row.name} - Transactions`);
+        setBucketTransactionsDialog(true);
       }
     }
   ];
@@ -86,12 +120,49 @@ function AccountsTable() {
   const rowActions = [
     {
       icon: 'eye-open',
-      click: (e, row, index) => {
+      click: (e, row) => {
         e.preventDefault();
-        alert(`Opened account row ${index}: ${row.name}`);
+        setAccountTransactions(row.transactions);
+        setAccountTransactionsDialogTitle(`${row.name} - Transactions`);
+        setAccountTransactionsDialog(true);
       }
     }
   ];
+
+  const transactionHeaders = [
+    {
+      key: 'description',
+      label: 'Description'
+    },
+    {
+      key: 'transactionDate',
+      label: 'Transaction Date'
+    },
+    {
+      key: 'amount',
+      label: 'Amount'
+    }
+  ];
+
+  const [accountTransactionsDialog, setAccountTransactionsDialog] = useState(false);
+  const [accountTransactionsDialogTitle, setAccountTransactionsDialogTitle] =
+    useState('Account Transactions');
+  const [accountTransactions, setAccountTransactions] = useState([]);
+
+  function closeAccountTransactionsDialog(e) {
+    e.preventDefault();
+    setAccountTransactionsDialog(false);
+  }
+
+  const [bucketTransactionsDialog, setBucketTransactionsDialog] = useState(false);
+  const [bucketTransactionsDialogTitle, setBucketTransactionsDialogTitle] =
+    useState('Bucket Transactions');
+  const [bucketTransactions, setBucketTransactions] = useState([]);
+
+  function closeBucketTransactionsDialog(e) {
+    e.preventDefault();
+    setBucketTransactionsDialog(false);
+  }
 
   return (
     <Card className="Page-form Accounts-table" elevation={Elevation.TWO}>
@@ -103,6 +174,36 @@ function AccountsTable() {
           rowActions={rowActions}
           expandedField={expandedField}></CollapseTable>
       </div>
+
+      <Dialog
+        className="Account-transactions-dialog"
+        title={accountTransactionsDialogTitle}
+        icon="list"
+        isOpen={accountTransactionsDialog}
+        onClose={closeAccountTransactionsDialog}>
+        <DialogBody>
+          <SimpleTable
+            headers={transactionHeaders}
+            rows={accountTransactions}
+            rowActions={[]}></SimpleTable>
+        </DialogBody>
+        <DialogFooter actions={<Button text="Close" onClick={closeAccountTransactionsDialog} />} />
+      </Dialog>
+
+      <Dialog
+        className="Bucket-transactions-dialog"
+        title={bucketTransactionsDialogTitle}
+        icon="list"
+        isOpen={bucketTransactionsDialog}
+        onClose={closeBucketTransactionsDialog}>
+        <DialogBody>
+          <SimpleTable
+            headers={transactionHeaders}
+            rows={bucketTransactions}
+            rowActions={[]}></SimpleTable>
+        </DialogBody>
+        <DialogFooter actions={<Button text="Close" onClick={closeBucketTransactionsDialog} />} />
+      </Dialog>
     </Card>
   );
 }
