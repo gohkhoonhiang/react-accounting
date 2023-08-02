786202f8d27efa5c27316a79238d7ae73df3cbe5
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2023-08-02 20:09:31 +0800

    Collapsible table and simple table components

---
+++ src/components/CollapseTable.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { Button, Collapse } from '@blueprintjs/core';
+import './CollapseTable.css';
+
+function CollapseTable({ headers, rows, rowActions, expandedField }) {
+  const [expandedRows, setExpandedRows] = useState({});
+
+  function toggleRow(e, index) {
+    e.preventDefault();
+
+    setExpandedRows((prev) => {
+      const updated = { ...prev };
+      if (prev[index]) {
+        delete updated[index];
+      } else {
+        updated[index] = true;
+      }
+
+      return updated;
+    });
+  }
+
+  function rowExpanded(index) {
+    return expandedRows[index];
+  }
+
+  return (
+    <div className="Collapse-table">
+      <div className="row row-heading">
+        <div className="col"></div>
+        {headers.map((header, i) => (
+          <div className="col" key={i}>
+            {header.label}
+          </div>
+        ))}
+        {rowActions.length > 0 ? <div className="col">Actions</div> : <></>}
+      </div>
+      {rows.map((row, j) => (
+        <div key={j}>
+          <div className="row">
+            <div className="col">
+              {rowExpanded(j) ? (
+                <Button small={true} icon="chevron-up" onClick={(e) => toggleRow(e, j)}></Button>
+              ) : (
+                <Button small={true} icon="chevron-down" onClick={(e) => toggleRow(e, j)}></Button>
+              )}
+            </div>
+            {headers.map((header, k) => (
+              <div className="col" key={k}>
+                {row[header.key]}
+              </div>
+            ))}
+            {rowActions.length > 0 ? (
+              <div className="col">
+                {rowActions.map((action, l) => (
+                  <Button
+                    small={true}
+                    icon={action.icon}
+                    onClick={(e) => action.click(e, row, j)}
+                    key={l}></Button>
+                ))}
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
+
+          <div className="row row-full">
+            <div className="col col-full">
+              <Collapse isOpen={rowExpanded(j)}>{row[expandedField]}</Collapse>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default CollapseTable;
---
+++ src/components/SimpleTable.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Button } from '@blueprintjs/core';
+import './SimpleTable.css';
+
+function SimpleTable({ headers, rows, rowActions }) {
+  return (
+    <div className="Simple-table">
+      <div className="row row-heading">
+        {headers.map((header, i) => (
+          <div className="col" key={i}>
+            {header.label}
+          </div>
+        ))}
+        {rowActions.length > 0 ? <div className="col">Actions</div> : <></>}
+      </div>
+      {rows.map((row, i) => (
+        <div className="row" key={i}>
+          {headers.map((header, j) => (
+            <div className="col" key={j}>
+              {row[header.key]}
+            </div>
+          ))}
+          {rowActions.length > 0 ? (
+            <div className="col">
+              {rowActions.map((action, j) => (
+                <Button
+                  small={true}
+                  icon={action.icon}
+                  onClick={(e) => action.click(e, row, i)}
+                  key={j}></Button>
+              ))}
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default SimpleTable;
